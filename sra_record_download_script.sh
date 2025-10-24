c4461e2b36e44b9a40bb557c731c1a45d1c28532
buddhathapa12 <buddhat645@gmail.com>
2025-10-24 00:18:47 -0500

    Added efetch for fasta and genbank file download

--- sra_record_download_script.sh
+++ sra_record_download_script.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-csv_file="sensitive_data/Sample_of_25_PAIRED_LibraryLayout_SRA_Records_Associated_with_Unique_Mitogenomes_except_y_chromosome.csv"
+csv_file="sensitive_data/sample_nucleotide_sra_mapped_infor_with_run.csv"
 temp_file=$(mktemp)
 # Skip header and write to temp file
 tail -n +2 "$csv_file" >"$temp_file"
 count=1
-while IFS=, read -r RUN _; do
+while IFS=, read -r Accession _SRA_Uid RUN _LibraryLayout; do
     echo "SRA: $RUN"
     # echo "Sample Name: $Samples"
-    # echo "Accession: $Accession"
-    fasterq-dump -O "SRA_data" -p -S "$RUN"
-    # efetch -db nuccore -id "$Accession" -format fasta >Data/Analysis2/step7_assembly_quality_metric/nucleotide_complete_genome/fasta/"$Accession".fasta
+    echo "Accession: $Accession"
+    fasterq-dump -O "genome_data/SRA_data" -p -S "$RUN"
+    efetch -db nuccore -id "$Accession" -format fasta >genome_data/complete_genome/fasta/"$Accession".fasta
+    efetch -db nuccore -id "$Accession" -format gb >genome_data/complete_genome/genbank/"$Accession".gb
+
     echo "$count" completed
     count=$((count + 1))
     echo "----------------------"
